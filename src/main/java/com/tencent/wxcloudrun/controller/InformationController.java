3e83f18acf361899d2522041e394b10eaaa9d00d
Locura <oncwnuBCBIAzD5PE9UctZEEBf9CE@git.weixin.qq.com>
2025-02-16 19:40:27 +0800

    获取所有信息的接口已经ok

--- src/main/java/com/tencent/wxcloudrun/controller/InformationController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/InformationController.java
@@ -3,7 +3,6 @@ package com.tencent.wxcloudrun.controller;
 import com.tencent.wxcloudrun.config.ApiResponse;
 import com.tencent.wxcloudrun.model.Information;
 import com.tencent.wxcloudrun.service.InformationService;
-import org.slf4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,15 +11,12 @@ import java.util.List;
 @RestController
 public class InformationController {
     final InformationService informationService;
-    final Logger logger;
 
-    public InformationController(InformationService informationService, Logger logger) {
+    public InformationController(InformationService informationService) {
         this.informationService = informationService;
-        this.logger = logger;
     }
     @GetMapping(value = "/api/informationAll")
     ApiResponse get() {
-        logger.info("/api/informationAll get request");
        List<Information> information = informationService.selectAllInformation();
         return ApiResponse.ok(information);
     }
